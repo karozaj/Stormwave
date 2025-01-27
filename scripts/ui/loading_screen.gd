4235a62f820110ad3cc524455a196addd0eadef0
karozaj <135151900+karozaj@users.noreply.github.com>
2025-01-27 17:12:03 +0100

    fix loading screen crash

--- scripts/ui/loading_screen.gd
+++ scripts/ui/loading_screen.gd
@@ -9,14 +9,17 @@ var progress:Array=[]
 
 var update:float=0.0
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ResourceLoader.load_threaded_request(Global.scene_to_load,"",true)
+	var error=ResourceLoader.load_threaded_request(Global.scene_to_load)
+	assert(error==OK)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	scene_load_status=ResourceLoader.load_threaded_get_status(Global.scene_to_load,progress)
+	#assert(scene_load_status == OK)
 	print(progress[0])
 	if progress[0]>update:
 		update=progress[0]
@@ -24,12 +27,14 @@ func _process(delta: float) -> void:
 		
 	if progress_bar.value<update*100:
 		progress_bar.value=(lerp(progress_bar.value,update*100,delta))
-	#progress_bar.value+=delta*0.2*(0.9-progress_bar.value)
+	progress_bar.value+=delta*0.2*(0.9-progress_bar.value)
 	
 	
 	if scene_load_status == ResourceLoader.THREAD_LOAD_LOADED:
 		var packed_scene:PackedScene=ResourceLoader.load_threaded_get(Global.scene_to_load)
-		get_tree().change_scene_to_packed(packed_scene)
+		assert(packed_scene!=null)
+		var error=get_tree().change_scene_to_packed(packed_scene)
+		assert(error==OK)
 	
 	#var progress=[]
 	#scene_load_status = ResourceLoader.load_threaded_get_status(Global.scene_to_load, progress)
